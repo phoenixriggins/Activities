465eb718ae1417b1d4fd1375689a059134e0cfd6
Phoenix Riggins <Phoenixriggins18@gmail.com>
2022-06-16 20:54:18 -0500

    add remaining 2.3 activities

---
+++ Week2/2.3/2.3.7-WhileLoops.js
@@ -0,0 +1,13 @@
+// Create a while loop that TOTALS the numbers from 1 to 100 by following these steps...
+
+// 1) create a variable to represent the current number
+let curNum = 100;
+// 2) create a variable to represent the current total
+let curTotal = 0;
+// 3) write a while loop that sums the numbers from 1 to 100
+while (i <= curNum){
+    curTotal += i;
+console.log(curTotal);
+    i++;
+}
+console.log(curTotal);
